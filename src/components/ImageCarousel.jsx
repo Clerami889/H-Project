fc182907f867730e6f7e6ee5b5a6b9d75030dab4
Clerami <ImClerami@gmail.com>
2023-04-08 14:16:12 +0700

    update

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -1,24 +1,35 @@
 import React, { useState } from "react";
-import { FiChevronRight } from "react-icons/fi";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import a from "./Gambar/Tes1.jpeg";
 import b from "./Gambar/Tes2.jpeg";
 import c from "./Gambar/Tes3.jpeg";
 const ImageCarousel = () => {
   const giss = [{ url: a }, { url: b }, { url: c }];
-
-  let i = 0;
-
-  const next = () => {
-    i += 1;
-    console.log(i);
-  };
+  const [i, setCounter] = useState(0);
 
   return (
-    <div>
-      <div className="card grid grid-rows-1 items-center inline-grid m-12 relative top-8 left-[30rem] bg-emerald-200 p-12">
-        <img src={giss[i].url} async className="w-[20rem] h-[30rem]" />
-        <button onClick={next}>
-          <FiChevronRight />
+    <div className="card flex-row justify-center inline-flex place-content-center m-12 bg-emerald-200 p-12">
+      <button>
+        <FiChevronLeft
+          className="w-12 h-12 bg-cover bg-orange-400"
+          onClick={() => {
+            setCounter(i - 1);
+          }}
+        />
+      </button>
+      <div>
+        <img
+          alt="ada"
+          src={giss[i].url}
+          className="inline 2xl:w-[30rem] 2xl:h-[40rem]"
+        />
+        <button>
+          <FiChevronRight
+            className="w-12 h-12 bg-cover bg-orange-400"
+            onClick={() => {
+              setCounter(i + 1);
+            }}
+          />
         </button>
       </div>
     </div>
