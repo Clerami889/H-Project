c21c34eb0e423948c1d98c04ecfc1552ee9b9163
Clerami <ImClerami@gmail.com>
2023-04-10 22:49:48 +0700

    update 0.2

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -1,33 +1,43 @@
 import React, { useState } from "react";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import a from "./Gambar/Tes1.jpeg";
-import b from "./Gambar/Tes2.jpeg";
+import b from "./Gambar/Tes2.jpg";
 import c from "./Gambar/Tes3.jpeg";
+
 const ImageCarousel = () => {
   const giss = [{ url: a }, { url: b }, { url: c }];
-  const [i, setCounter] = useState(0);
-
+  const [i, setImage1] = useState(0);
+  const [o, setImage2] = useState(1);
   return (
-    <div className="card flex-row justify-center inline-flex place-content-center m-12 bg-emerald-200 p-12">
-      <button>
-        <FiChevronLeft
-          className="w-12 h-12 bg-cover bg-orange-400"
-          onClick={() => {
-            setCounter(i - 1);
-          }}
-        />
-      </button>
-      <div>
+    <div className="flex max-w-6xl mx-auto ">
+      <div className="container inline-flex justify-center gap-4 place-content-center m-12 bg-neutral-700  py-12  rounded-xl">
+        <button>
+          <FiChevronLeft
+            className="w-12 h-12 bg-cover bg-stone-200/30"
+            onClick={() => {
+              setImage1(i - 1);
+              setImage2(o - 1);
+            }}
+          />
+        </button>
+
         <img
-          alt="ada"
+          alt=""
           src={giss[i].url}
-          className="inline 2xl:w-[30rem] 2xl:h-[40rem]"
+          className="inline 2xl:w-[25rem] 2xl:h-[35rem]"
+        />
+
+        <img
+          alt=""
+          src={giss[o].url}
+          className="inline 2xl:w-[25rem] 2xl:h-[35rem]"
         />
         <button>
           <FiChevronRight
-            className="w-12 h-12 bg-cover bg-orange-400"
+            className="w-12 h-12 bg-cover bg-stone-200/30"
             onClick={() => {
-              setCounter(i + 1);
+              setImage2(o + 1);
+              setImage1(i + 1);
             }}
           />
         </button>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,28 +3,28 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="">
-      <nav className="flex flex-row bg-slate-600 justify-around h-32 items-center ">
+      <nav className="flex flex-row bg-zinc-800 justify-around h-32 items-center ">
         <section className="flex flex-row ">
-          <div className="bg-[url('../public/android-chrome-512x512.jpeg')] h-14 w-14 bg-no-repeat bg-contain pl-2"></div>
-          <h1 className="font-extralight text-[2rem] text-slate-200 pl-2">
+          <div className="bg-[url('../public/android-chrome-512x512.jpeg')] h-14 w-14 bg-no-repeat bg-contain pl-2 "></div>
+          <h1 className="font-extralight md:text-[2rem] text-slate-200 md:pl-2 min-[400px]:text-[1rem]">
             Heroes Palembang
           </h1>
         </section>
 
-        <ul className="flex flex-row list-none space-x-6 text-slate-200 text-[1.2rem] ">
-          <li className="hover:text-slate-100">
+        <ul className="flex flex-row list-none md:space-x-6 text-slate-200 md:text-[1.2rem] min-[400px]:">
+          <li className="hover:text-slate-100 hover:underline hover:decoration-solid">
             <a href="google.com">Home</a>
           </li>
-          <li className="hover:text-slate-100">
+          <li className="hover:text-slate-100 hover:underline hover:decoration-solid">
             <a href="facebook.com">Events</a>
           </li>
-          <li className="hover:text-slate-100">
+          <li className="hover:text-slate-100 hover:underline hover:decoration-solid">
             <a href="youtube.com">Heroes Leader</a>
           </li>
-          <li className="hover:text-slate-100">
+          <li className="hover:text-slate-100 hover:underline hover:decoration-solid">
             <a href="https://youtu.be/S2Cti12XBw4">About</a>
           </li>
-          <li className="hover:text-slate-100">
+          <li className="hover:text-slate-100 hover:underline hover:decoration-solid">
             <a href="chatgpt.com">Join Us</a>
           </li>
         </ul>
